18f51a6f0bef0b073b1cde712a243e41482864a8
Scarlett <lopezscarlett103096@yahoo.com>
2022-02-25 14:40:59 -0800

    commit tree f2a2c8c2d90e642a4f69e69c0b5809ff75847fd8

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if(block > 42) {
+        return block-42;
+    } else {
+        return 42-block;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    let distance = distanceFromHqInBlocks(blocks);
+    return distance * 264
+}
+
+function distanceTravelledInFeet(block1, block2) {
+    if(block1>block2) {
+        return (block1-block2) * 264
+    } else {
+        return (block2-block1) * 264
+    }
+}
+
+function calculatesFarePrice(block1, block2) {
+    let feetDistance = distanceTravelledInFeet(block1, block2);
+
+    if(feetDistance<400) {
+        return 0
+    } else if(feetDistance>=400 && feetDistance<2000) {
+        return 2.56
+    } else if (ride <= 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+    
+}
